e3d71400cbdc78fd10394f27074efab8fcc268e6
Pavel Kovalchuk <x5bart@yandex.ru>
2020-03-27 14:30:46 +0300

    Initial commit

---
+++ app/src/main/java/com/exampler/autoupdater/api/FileDownloadClient.kt
@@ -0,0 +1,16 @@
+package com.exampler.autoupdater.api
+
+
+import com.exampler.autoupdater.LATEST_VERSION_URL
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FileDownloadClient {
+
+    @GET(LATEST_VERSION_URL)
+    fun downloadLastVersionInfo(): Call<ResponseBody?>?
+
+
+
+}
